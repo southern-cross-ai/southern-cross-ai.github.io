186816f53cfac9a0ac7315f8737e2c604aa83a85
Matthew Altenburg <matthew.altenburg@anu.edu.au>
2025-01-14 17:33:17 +1100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const element = document.querySelector('.your-element');
-    if (element) {
-        element.addEventListener('click', () => {
-            console.log('Element clicked!');
-        });
-    }
+    console.log('JavaScript is ready!');
+    // Add your future scripts here.
 });
+
 
